97fce2b377cd0c646a3fcd1b0480e95765ef6ab3
nithinkonakalla <nithinkonakalla2357@gmail.com>
2024-12-20 23:45:06 +0530

    added another project

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -10,8 +10,8 @@ const projects = [
     title: "AI SaaS Text-to-Image Generator App",
     description: "A Text-to-Image Generator App powered by AI, allowing users to create images from text prompts. The app features an integrated credit system, where users can generate images based on their available credits. Users can purchase additional credits through an online payment gateway Razorpay. The platform also includes secure user authentication with JWT (JSON Web Token) for safe login and user management.",
     image: "/placeholder.svg?height=200&width=300",
-    github: "https://github.com/yourusername/project1",
-    demo: "https://project1-demo.com",
+    github: "https://github.com/nithinkonakalla/AI-SaaS-Text-to-Image-Generator-App",
+    demo: "https://ai-saa-s-text-to-image-generator-app-afel-1dnjy7uxj.vercel.app/",
     tech: ["React", "Node.js", "MongoDB", "Express","Razorpay","JWT Auth","ClipDrop Api"]
   },
   {
